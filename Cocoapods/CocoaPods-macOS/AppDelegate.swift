73f926829a7e9678aca98be72881d888946cfcc7
ApolloZhu <apollo.zhu@xiangwushuo.com>
2019-08-23 14:59:00 +0800

    Move CocoaPods into folder

---
+++ Cocoapods/CocoaPods-macOS/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CocoaPods-macOS
+//
+//  Created by ApolloZhu on 2019/8/23.
+//  Copyright © 2019 EFPrefix. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Cocoapods/CocoaPods-macOS/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  CocoaPods-macOS
+//
+//  Created by ApolloZhu on 2019/8/23.
+//  Copyright © 2019 EFPrefix. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    @available(OSX 10.10, *)
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
