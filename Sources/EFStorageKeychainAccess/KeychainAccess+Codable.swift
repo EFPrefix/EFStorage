520ae8e603cef15cb753f95c1359f12af0db5e67
ApolloZhu <public-apollonian@outlook.com>
2021-03-24 12:20:49 -0400

    Fix KeychainAccessStorable + Codable default implementation

--- Sources/EFStorageKeychainAccess/KeychainAccess+Codable.swift
+++ Sources/EFStorageKeychainAccess/KeychainAccess+Codable.swift
@@ -9,10 +9,10 @@ import KeychainAccess
 import Foundation
 
 public extension KeychainAccessStorable where Self: Codable {
-    func asKeychainStorable() -> Result<AsIsKeychainAccessStorable, Error> {
+    func asKeychainAccessStorable() -> Result<AsIsKeychainAccessStorable, Error> {
         return Result { try JSONEncoder().encode(self) }
             .mapError { print($0);return $0 }
-            .flatMap { $0.asKeychainStorable() }
+            .flatMap { $0.asKeychainAccessStorable() }
     }
     static func fromKeychain(_ keychain: Keychain, forKey key: String) -> Self? {
         return Data.fromKeychain(keychain, forKey: key).flatMap {
